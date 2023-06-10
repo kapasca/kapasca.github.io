6fc368f755770f201c6a71b6cf751ae59c603f61
Kapasca <pascadriyarkara@gmail.com>
2023-06-11 00:41:09 +0700

    Initial Commit - ANGKIPU

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <title>ANGKIPU</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/fontawesome/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/fontawesome/css/solid.css">
+    <link rel="stylesheet" href="assets/css/pattern.css-master/dist/pattern.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Moirai+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div class="decoration-bar">
+            <div class="decoration-column" style="background-color: red;"></div>
+            <div class="decoration-column" style="background-color: green;"></div>
+            <div class="decoration-column" style="background-color: blue;"></div>
+            <div class="decoration-column" style="background-color: yellow;"></div>
+            <div class="decoration-column" style="background-color: orange;"></div>
+            <div class="decoration-column" style="background-color: purple;"></div>
+            <div class="decoration-column" style="background-color: brown;"></div>
+            <div class="decoration-column" style="background-color: gray;"></div>
+        </div>
+        <div class="header-content pattern">
+            <div class="title">
+                ANGKIPU
+            </div>
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="#"><i class="fas fa-home"></i>&nbsp; Home</a></li>
+                        <li><a href="#"><i class="fas fa-user"></i>&nbsp; Bio</a></li>
+                        <li><a href="#"><i class="fas fa-photo-video"></i>&nbsp; Artwork</a></li>
+                        <li><a href="#"><i class="fas fa-suitcase"></i>&nbsp; Project</a></li>
+                        <li><a href="#"><i class="fas fa-id-card"></i>&nbsp; Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'header.php'; ?>
+
+<main>
+    <div class="section-primary">
+        <div class="show-banner">
+
+            <div class="virtual-light-box">
+
+            </div>
+            <div class="button-container">
+                <button class="play-button" title="Putar slideshow secara otomatis setiap 5 detik!"><i class="fas fa-play"></i></button>
+                <button class="pause-button" title="Hentikan pemutaran slideshow otomatis!"><i class="fas fa-pause"></i></button>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-secondary">
+        <div class="banner-list"><img src="assets/img/home/angkipu-x01.jpg" alt="angkipu-x01"></div>
+        <div class="banner-list"><img src="assets/img/home/angkipu-x02.jpg" alt="angkipu-x02"></div>
+        <div class="banner-list"><img src="assets/img/home/angkipu-x03.jpg" alt="angkipu-x03"></div>
+        <div class="banner-list"><img src="assets/img/home/angkipu-x04.jpg" alt="angkipu-x04"></div>
+        <div class="banner-list"><img src="assets/img/home/angkipu-x05.jpg" alt="angkipu-x05"></div>
+        <div class="banner-list"><img src="assets/img/home/angkipu-x06.jpg" alt="angkipu-x06"></div>
+        <div class="banner-list"><img src="assets/img/home/angkipu-x07.jpg" alt="angkipu-x07"></div>
+        <div class="banner-list"><img src="assets/img/home/angkipu-x08.jpg" alt="angkipu-x08"></div>
+    </div>
+</main>
+
+<script src="script.js"></script>
+
+<?php include 'footer.php'; ?>
